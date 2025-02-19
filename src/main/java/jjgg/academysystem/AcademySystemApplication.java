4446f2822ff6503c61cd88163910c35366f80937
(JG)² <juanjguzman15@gmail.com>
2025-02-19 09:58:47 -0500

    Initial commit

---
+++ src/main/java/jjgg/academysystem/AcademySystemApplication.java
@@ -0,0 +1,13 @@
+package jjgg.academysystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademySystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademySystemApplication.class, args);
+	}
+
+}
