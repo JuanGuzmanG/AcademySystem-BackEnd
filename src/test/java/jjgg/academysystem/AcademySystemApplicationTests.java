57d3a4a9b5a103293b30f51e88c0540339ec8eb1
(JG)² <juanjguzman15@gmail.com>
2025-02-19 12:32:31 -0500

    fc
    first commit

--- src/test/java/jjgg/academysystem/AcademySystemApplicationTests.java
+++ src/test/java/jjgg/academysystem/AcademySystemApplicationTests.java
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class AcademySystemApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
